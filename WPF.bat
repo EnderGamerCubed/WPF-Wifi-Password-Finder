0d54b61d92112363462d0aebdb4bec72503c13ed
EnderGamerCubed <derpbanana01@gmail.com>
2023-10-25 14:25:12 -0600

    Major Release 1.0
    Now has User input.

--- WPF.bat
+++ WPF.bat
@@ -11,6 +11,8 @@ echo.
 echo          WELCOME %USERNAME%
 echo          TO WPF
 echo.
+echo          PASSWORD = KEY CONTENT
+echo.
 echo.
 echo /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
 echo.
@@ -23,11 +25,11 @@ cls
 color C
 NETSH WLAN SHOW PROFILE
 echo.
-pause
 
-cls
 color 4
 echo.
 :1
-NETSH WLAN SHOW PROFILE "<INSERT WIFI NAME HERE>" KEY=CLEAR > "C:\Users\%USERNAME%\Downloads\WIFI-password-finder-main\WPF Results.txt"
+set /P input="Input Wifi Name: "
+NETSH WLAN SHOW PROFILE %input% KEY=CLEAR
+NETSH WLAN SHOW PROFILE %input% KEY=CLEAR > "C:\Users\%USERNAME%\Downloads\WPF-Wifi-Password-Finder-main\WPF-Results.txt"
 pause
